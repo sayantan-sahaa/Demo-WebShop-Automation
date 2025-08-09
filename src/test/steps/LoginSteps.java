af8e66a329f43e353b823b90bb4bcd90ba94bb4a
Sayantan-Saha-QA <sayantan.saha@outlook.in>
2025-08-09 22:24:48 +0530

    commit changes

--- src/test/steps/LoginSteps.java
+++ src/test/steps/LoginSteps.java
@@ -2,15 +2,10 @@ package steps;
 
 import static pages.HomePage.*;
 import static pages.LoginPage.*;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.*;
 
 import base.Base;
 import io.cucumber.java.en.*;
-import pages.HomePage;
 
 import static utils.CommonUtils.*;
 
@@ -18,9 +13,6 @@ public class LoginSteps extends Base{
 
     @Test(groups = {"Login"})
     public void completeLoginFlow(){
-
-        PageFactory.initElements(driver.get(), HomePage.class);
-
         clickLoginURL().enterValidEmail()
         .enterValidPassword().clickLoginButton().
         verifySuccessfulLogin();
@@ -28,7 +20,6 @@ public class LoginSteps extends Base{
     
     @Given("the user is on the Demo Web Shop login page")
     public LoginSteps clickLoginURL(){
-        WebElement login = getDr().findElement(By.cssSelector("a.ico-login"));
         loginURL.click();
         return this;
     }
@@ -48,7 +39,6 @@ public class LoginSteps extends Base{
     @And("the user clicks on the login button")
     public LoginSteps clickLoginButton() {
         scrollToElement(loginButton);
-        //new Actions(getDr()).sendKeys(Keys.PAGE_DOWN).perform();
         loginButton.click();
         return this;
     }
